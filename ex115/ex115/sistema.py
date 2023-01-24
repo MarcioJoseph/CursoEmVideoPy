faf0b6f7b00f8097506b07f3da1fbdd00b641c93
Márcio José da Silva <112825435+MarcioJoseph@users.noreply.github.com>
2023-01-24 15:38:30 -0300

    Segunda parte do ex115
    Criando e fazendo acesso de arquivos.

---
+++ ex115/ex115/sistema.py
@@ -0,0 +1,22 @@
+from ex115.lib.interface import *
+from time import sleep
+from ex115.lib.arquivo import *
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar novas Pessoas', 'Sair do Sistema'])
+    if resposta == 1:
+        # Opção de listar o conteudo de um arquivo
+        lerArquivo(arq)
+    elif resposta == 2:
+        cabeçalho('Opção 2')
+    elif resposta == 3:
+        cabeçalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(2)
