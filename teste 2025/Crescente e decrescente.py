4d147d47c9ce9787d1005dd1b2fb466c1942773b
MarcioJoseph <112825435+MarcioJoseph@users.noreply.github.com>
2025-03-14 18:38:46 -0300

    Exercícios de Python do Beecrowd

---
+++ teste 2025/Crescente e decrescente.py
@@ -0,0 +1,10 @@
+while True:
+    valores = [int(i) for i in input().split()]
+    x, y = valores
+    if x == y:
+        break
+    elif x > y:
+        print('Decrescente')
+    else:
+        print('Crescente')
+
